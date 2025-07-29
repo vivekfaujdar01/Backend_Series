89c88ee75cf5d5c3e92522d43bc48ba1393786a3
Vivek <152777809+vivekfaujdar01@users.noreply.github.com>
2025-07-29 09:25:40 +0530

    Database Connected Successfully

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
-console.log("hii from backedn/src/index.js");
+import dotenv from 'dotenv';
+import {app} from './app.js';
+import connectDB from './db/index.js';import { connect } from 'mongoose';
+
+dotenv.config({
+    path: './src/.env',
+});
+
+const PORT = process.env.PORT || 3001;
+
+connectDB()
+  .then(() => {
+    console.log('Database connected successfully');
+    return app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });// Start the server after successful DB connection
+  })
+  .catch((error) => {
+    console.error('Database connection failed:', error);
+    process.exit(1);
+  });
+
+
